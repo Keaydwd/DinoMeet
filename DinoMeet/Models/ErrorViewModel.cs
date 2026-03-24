9a0968d49c1585814818ba06bd3cfda4f60f58dd
Аліна Сергеєва <alina.serheieva@oa.edu.ua>
2026-03-24 20:33:00 +0200

    Project Created

---
+++ DinoMeet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DinoMeet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
